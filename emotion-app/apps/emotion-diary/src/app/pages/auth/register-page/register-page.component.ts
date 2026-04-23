d4d21af2de8bcfc5dafb060ee73bca72beebe5cb
dashk <burckina.anna@yandex.ru>
2026-04-23 12:47:24 +0300

    feat(pages): add initial page scaffolds

---
+++ emotion-app/apps/emotion-diary/src/app/pages/auth/register-page/register-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterPageComponent {}
