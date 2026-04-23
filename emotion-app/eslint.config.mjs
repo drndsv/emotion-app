f9ff8306bfeb6f304554cf1107c17c62965375e0
dashk <burckina.anna@yandex.ru>
2026-04-23 10:16:49 +0300

    chore(app): add prettier and eslint configs

--- emotion-app/eslint.config.mjs
+++ emotion-app/eslint.config.mjs
@@ -1,11 +1,18 @@
 import nx from '@nx/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc'],
+    ignores: [
+      '**/node_modules',
+      '**/dist',
+      '**/out-tsc',
+      '**/coverage',
+      '**/.nx',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -36,7 +43,59 @@ export default [
       '**/*.cjs',
       '**/*.mjs',
     ],
-    // Override or add rules here
-    rules: {},
+    plugins: {
+      import: importPlugin,
+    },
+
+    rules: {
+      curly: ['error', 'all'],
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      'import/no-duplicates': 'error',
+    },
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+    rules: {
+      '@typescript-eslint/member-ordering': [
+        'error',
+        {
+          default: [
+            'private-instance-field',
+            'private-static-field',
+
+            'public-instance-field',
+            'public-static-field',
+
+            'constructor',
+
+            'public-instance-method',
+            'public-static-method',
+
+            'private-instance-method',
+            'private-static-method',
+          ],
+        },
+      ],
+    },
   },
 ];
