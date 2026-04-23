11c8f964c99a4cd432fc202f3ad87aacc8bb6e9a
dashk <burckina.anna@yandex.ru>
2026-04-23 10:39:27 +0300

    chore(app): add taiga ui

--- emotion-app/apps/emotion-diary/src/app/app.config.ts
+++ emotion-app/apps/emotion-diary/src/app/app.config.ts
@@ -3,9 +3,10 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTaiga } from "@taiga-ui/core";
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes), provideTaiga()],
 };
--- emotion-app/apps/emotion-diary/src/app/app.ts
+++ emotion-app/apps/emotion-diary/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TuiRoot } from "@taiga-ui/core";
 
 @Component({
-  imports: [ RouterModule],
+  imports: [RouterModule, TuiRoot],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.less',
