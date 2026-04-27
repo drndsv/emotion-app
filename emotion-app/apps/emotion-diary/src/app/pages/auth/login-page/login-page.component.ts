40dc2ed9f91e699646cdb6dbbaccf0cf29a4d4e6
dashk <burckina.anna@yandex.ru>
2026-04-27 18:30:01 +0300

    feat(auth): add login page layout

--- emotion-app/apps/emotion-diary/src/app/pages/auth/login-page/login-page.component.ts
+++ emotion-app/apps/emotion-diary/src/app/pages/auth/login-page/login-page.component.ts
@@ -1,10 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { TuiButton, TuiInput } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink, TuiInput, ReactiveFormsModule, TuiButton],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  protected readonly emailControl = new FormControl('', {
+    nonNullable: true,
+  });
+
+  protected readonly passwordControl = new FormControl('', {
+    nonNullable: true,
+  });
+}
