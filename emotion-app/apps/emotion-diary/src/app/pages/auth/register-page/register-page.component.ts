fa94ce4f06cbd074426265cc4cf83610a92199a1
dashk <burckina.anna@yandex.ru>
2026-04-28 10:20:23 +0300

    feat(auth): add register page

--- emotion-app/apps/emotion-diary/src/app/pages/auth/register-page/register-page.component.ts
+++ emotion-app/apps/emotion-diary/src/app/pages/auth/register-page/register-page.component.ts
@@ -1,10 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { TuiButton, TuiInput } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [RouterLink, TuiInput, ReactiveFormsModule, TuiButton],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  protected readonly nameControl = new FormControl('', {
+    nonNullable: true,
+  });
+
+  protected readonly emailControl = new FormControl('', {
+    nonNullable: true,
+  });
+
+  protected readonly passwordControl = new FormControl('', {
+    nonNullable: true,
+  });
+}
