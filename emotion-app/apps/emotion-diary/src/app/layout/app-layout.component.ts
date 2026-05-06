69d864cea9b0ee162a05a39a11e4ccb3b53a1013
dashk <burckina.anna@yandex.ru>
2026-05-06 16:37:14 +0300

    refactor(app): delete unused imports

--- emotion-app/apps/emotion-diary/src/app/layout/app-layout.component.ts
+++ emotion-app/apps/emotion-diary/src/app/layout/app-layout.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TuiNavigation } from '@taiga-ui/layout';
 
 import { AppHeaderComponent } from './header/app-header.component';
 
 @Component({
   selector: 'app-app-layout.component',
-  imports: [AppHeaderComponent, RouterOutlet, TuiNavigation],
+  imports: [AppHeaderComponent, RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
